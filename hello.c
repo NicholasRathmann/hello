164d280dfc90c5649651d8f29a8f81e33fe895bf
NicholasRathmann <nrathmann2@huskers.unl.edu>
2023-08-24 09:52:18 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+Author: Nicholas Rathmann
+Date: 2023-08-24
+
+A simple hello world program in C
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc char **argv){
+    printf("Nick Rathmann\n");
+    return 0;
+}
